1e763e59051cfd9806a2fd0dea5f05468182d106
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-04 20:11:18 -0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.scss'
+import Home from './pages/Home/Home.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import './Home.styles.scss'
+
+function Home() {
+  return <h1 className="teste">Teste</h1>
+}
+
+export default Home
