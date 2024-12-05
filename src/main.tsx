e71bcc48d17937c9e91a305e949be169bca0b008
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-05 08:44:45 -0300

    chore: add react usecontext to provide database info

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './GlobalStyles.scss'
 import Home from './pages/Home/Home.tsx'
+import { GetPostsProvider } from './context/PostsContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <GetPostsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </GetPostsProvider>
   </StrictMode>,
 )
