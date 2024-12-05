7d8163d9e1aafc14f3d538c6a58979450ef50091
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-04 21:13:18 -0300

    styles: add general styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import './GlobalStyles.scss'
 import Home from './pages/Home/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
