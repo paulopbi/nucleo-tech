d643d639c7c06dd6bbabcb363537bdc889309daf
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-06 09:07:29 -0300

    feat: add loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import './Loading.style.scss'
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loading"></div>
+      <h4>Carregando...</h4>
+    </div>
+  )
+}
+
+export default Loading
