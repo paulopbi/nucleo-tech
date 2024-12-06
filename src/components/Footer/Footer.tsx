654ff1656a9eb7c9ab29d421a7e7327cb1886797
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-06 11:17:02 -0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import { Github, Linkedin } from 'lucide-react'
+import './Footer.style.scss'
+
+function Footer() {
+  const yearNow = new Date().getFullYear()
+  return (
+    <footer className="footer">
+      <div className="footer-content container">
+        <div>
+          <p>
+            &copy; {yearNow} - Criado por Paulo Victor, Alguns direitos
+            reservados.
+          </p>
+        </div>
+        <div className="footer-icons">
+          <a
+            href="https://www.linkedin.com/in/paulopbi/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Linkedin />
+          </a>
+          <a
+            href="https://github.com/paulopbi"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Github />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
