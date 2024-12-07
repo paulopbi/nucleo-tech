6685a1bbd13e055497ef67fdda02f234588eaaff
Paulo Victor Silva <paulpbi16@gmail.com>
2024-12-06 21:39:38 -0300

    feat: allow to create articles

---
+++ src/helpers/createPost.ts
@@ -0,0 +1,44 @@
+export default async function createPost(
+  title: string,
+  description: string,
+  post: string,
+  image: string,
+) {
+  let status: Response | null = null
+  try {
+    const req = await fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        post,
+        image,
+        date: new Date().toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: 'numeric',
+        }),
+      }),
+    })
+
+    if (!req.ok) {
+      throw new Error(
+        `Algo deu errado ao enviar os dados, status? ${req.status}`,
+      )
+    }
+
+    status = req
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message)
+      status = null
+    }
+  }
+
+  return {
+    status,
+  }
+}
